1e762274861c030239e38a74d544bc746e09e3fc
k4kotlin <k4kotlin@gmail.com>
2018-10-18 17:47:04 +0530

    how create component ng generate component student/ ng g c student and how to create model ng generate module studentPortal and also create component our module ng g c studentPortal/studentlogin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { StudentloginComponent } from './studentPortal/studentlogin/studentlogin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    StudentloginComponent
   ],
   imports: [
     BrowserModule
