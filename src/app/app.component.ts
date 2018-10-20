22bc828b0faa38adb95acca265631d6587e03f34
k4kotlin <k4kotlin@gmail.com>
2018-10-20 13:03:05 +0530

    ngswitch with exmpales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
+   templateUrl: './app.component.html',
   // template mai hum direct text echo karva skte hai but templateurl mai file print karava skte hai
-  template : '<h1>Hello man</h1>',
+  // template : '<h1>Hello man</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-app';
+ public choice = '';
+
+  changeValue(getmyvalue: any) {
+    this.choice = getmyvalue.target.value;
+  }
 }
