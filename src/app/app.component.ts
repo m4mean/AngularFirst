cd6def76232351f69e83c5038b626ca680dfba37
k4kotlin <k4kotlin@gmail.com>
2018-10-20 16:59:59 +0530

    grouping ngfor we use two ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,42 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
   // template mai hum direct text echo karva skte hai but templateurl mai file print karava skte hai
-  template : '<h1>Hello man</h1>',
+  // template : '<h1>Hello man</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-app';
+  peoplebycountry: any[] = [
+    {
+      'country' : 'india',
+      'people' : [
+        {
+          'name' : 'rohan',
+        },
+        {
+          'name' : 'sonali',
+        },
+        {
+          'name' : 'akshita',
+        },
+        {
+          'name' : 'gifty',
+        }
+        ]
+    },
+    {
+      'country' : 'usa',
+      'people' : [
+        {
+          'name' : 'john',
+        },
+        {
+          'name' : 'camilli',
+        }
+        ]
+    },
+        ];
+
 }
