3cc492ca05e9d2605fd9a6a0328a0ca5ff45336c
k4kotlin <k4kotlin@gmail.com>
2018-10-20 11:17:45 +0530

    with ngif and then and else with examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
   // template mai hum direct text echo karva skte hai but templateurl mai file print karava skte hai
-  template : '<h1>Hello man</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-app';
+  isValid = true;
+  withElsePart = true;
+  withElseThenPart = true;
+
+  changeValue(b: boolean) {
+    this.isValid =  b;
+    this.withElsePart = b;
+    this.withElseThenPart = b;
+  }
 }
