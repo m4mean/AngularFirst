e01d6dbdb7334c9c3a6557a05628c663e1911bbe
k4kotlin <k4kotlin@gmail.com>
2018-10-20 10:11:40 +0530

    directives short examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   // templateUrl: './app.component.html',
   // template mai hum direct text echo karva skte hai but templateurl mai file print karava skte hai
-  template : '<h1>Hello man</h1>',
+  template : `<h1>Hello man</h1> <button (click)="btnClick()">Click me</button>
+  <!--ngIf this div is hide and show depend on variable this is structural-->
+            <p *ngIf="chif">Hello man how are you</p>
+            <div [ngStyle]="{'color':'red','font-weight':'bold'}">this ngStyle examples</div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-app';
+  chif = false;
+  // this is components directives
+  btnClick() {
+    alert('working');
+  }
+
 }
