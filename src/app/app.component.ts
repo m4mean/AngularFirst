0e3675f732de791ed32e06cd5ead79eeb355548d
k4kotlin <k4kotlin@gmail.com>
2018-10-20 14:36:17 +0530

    ngfor with array list and even old and index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
   // template mai hum direct text echo karva skte hai but templateurl mai file print karava skte hai
-  template : '<h1>Hello man</h1>',
+  // template : '<h1>Hello man</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-app';
+  people: any[] = [
+    {
+      'name' : 'rohan dhiman'
+    },
+    {
+      'name' : 'sonali dhiman'
+    },
+    {
+      'name' : 'nisha dhiman'
+    },
+    {
+      'name' : 'vaishali dhiman'
+    },
+    {
+      'name' : 'gifty dhiman'
+    }
+  ];
 }
