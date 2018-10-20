0fb6fd7a19e613b90232dddea08643141633916c
k4kotlin <k4kotlin@gmail.com>
2018-10-20 17:22:05 +0530

    ngstyle two way use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,38 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  // template mai hum direct text echo karva skte hai but templateurl mai file print karava skte hai
-  template : '<h1>Hello man</h1>',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-app';
+  peoples: any[] = [
+    {
+      'name' : 'rohan',
+      'country' : 'india'
+    },
+    {
+      'name' : 'nisha',
+      'country' : 'canada'
+    },
+    {
+      'name' : 'camilli',
+      'country' : 'german'
+    },
+    {
+      'name' : 'akshita',
+      'country' : 'india'
+    }
+  ];
+
+  getColor(country) {
+    switch (country) {
+      case 'india':
+        return 'green';
+      case 'canada':
+        return 'red';
+      case 'german':
+        return 'blue';
+    }
+  }
 }
