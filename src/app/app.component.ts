75f1c90c2ea4c9001cf222dafc857f688c543733
k4kotlin <k4kotlin@gmail.com>
2018-10-21 12:42:48 +0530

    all way binding with exmaples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  // template mai hum direct text echo karva skte hai but templateurl mai file print karava skte hai
-  template : '<h1>Hello man</h1>',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-app';
+  // interpolation binding
+  colspan: number = 3;
+  name = 'rohan hacker';
+  gffirst: string = 'nisha';
+  gflast:string = 'dhiman';
+
+  // attribute binding
+  bdr: number = 5;
+  wid: number = 350;
+  hig: number = 150;
+
+  myonClick() {
+    alert('alert event');
+  }
+
+  data : string = '';
+  dataOne: string = 'rohan';
+
 }
