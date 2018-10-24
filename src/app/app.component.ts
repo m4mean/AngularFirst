e5ea1c01849a3ddc2d8a33733959332c9580555e
k4kotlin <k4kotlin@gmail.com>
2018-10-24 23:32:55 +0530

    dependency injection first create service ng generate service myservice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import {MyserviceService} from './myservice.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MyserviceService]
 })
 export class AppComponent {
   deptoobj: any[];
   title = 'my-app';
   constructor(private _service: MyserviceService) {
-    this.deptoobj = this._service.dept;
+    this.deptoobj = this._service.denarray;
   }
 }
