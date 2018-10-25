a8c7cae6c17580652d0750d00a038e0e45e1d3bb
k4kotlin <k4kotlin@gmail.com>
2018-10-25 17:48:38 +0530

    template driven form with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {first} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  Register(regForm: any) {
+    // you can also use debugger
+    // debugger;
+    // console.log(regForm);
+
+    const firstname = regForm.controls.firstname.value;
+    const lastname = regForm.controls.lastname.value;
+    const email = regForm.controls.email.value;
+
+    console.log(`firs name ${firstname} last name ${lastname} email ${email}`);
+
+  }
+
 }
